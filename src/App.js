ae3bed79f21db5376104642f9182146ab41e0c9e
ZhivkoAnd <Zhivko_Andreev@abv.bg>
2022-02-08 16:40:09 +0100

    asd

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [coins, setCoins] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const responce = await fetch(
+        `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false`
+      );
+      const fetchedData = await responce.json();
+      setCoins(fetchedData);
+    };
+    fetchData();
+  }, []);
+
+  const filteredCoins = coins.filter((e) => {
+    return e.id === "bitcoin" || e.id === "dogecoin";
+  });
+
+  console.log(filteredCoins);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {filteredCoins.map((coin) => {
+        return (
+          <div key={coin.id}>
+            {coin.name}
+            {coin.current_price}
+            <div>
+              {coin.id === "bitcoin" ? coin.current_price * 0.01001578 : null}
+            </div>
+            <div>
+              {coin.id === "dogecoin" ? coin.current_price * 543 : null}
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
 }
